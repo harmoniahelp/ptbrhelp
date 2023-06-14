7377beff0f402b8f13c217deff5ec0b6b0df0b07
harmoniahelp <135713257+harmoniahelp@users.noreply.github.com>
2023-06-14 14:48:45 -0300

    Add files via upload

---
+++ MANIFEST.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Build-Date' => 'Mon, 12 Jun 23 16:08:01 +0000',
+  'Phrases-Version' => NULL,
+  'Build-Version' => 'v1.17.3-20-ge4bfb00d',
+  'Build-Major-Version' => '1.17',
+  'Language' => NULL,
+  'Id' => 'lang:pt_BR',
+  'Last-Revision' => NULL,
+  'Version' => 0,
+);
